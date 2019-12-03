0e82413fd023d004fb4e68a81d77054db9ba8c93
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-03 17:52:15 -0500

    first commit

---
+++ src/main/java/com/unicon/api/serviceapiplanta/ServiceApiPlantaApplication.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapiplanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiPlantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiPlantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicon/api/serviceapiplanta/ServiceApiPlantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapiplanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiPlantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
